1601d2178d461c10634513c8bb32b0f8fb91bc06
Vaerian <vaeriann@gmail.com>
2021-07-11 16:48:41 -0500

    Initial commit

---
+++ src/main/java/com/cumulusmc/unbound/Unbound.java
@@ -0,0 +1,10 @@
+package com.cumulusmc.unbound;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Unbound implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Let's get unbound!");
+	}
+}
